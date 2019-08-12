8c8fed916b26c8a1c55d9e1e8a1b8b8f7f84768a
Robert Tam <rsh.tam@gmail.com>
2019-08-12 15:22:13 +0100

    Add FilmBox component and check rendering

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FilmBox extends Component {
+  render() {
+    return(
+      <h1>Hi, I am a FilmBox</h1>
+    );
+  }
+}
+
+export default FilmBox;
