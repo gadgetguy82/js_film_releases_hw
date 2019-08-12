6b98050c74bb4eeef359fc956407f33cb3a74188
Robert Tam <rsh.tam@gmail.com>
2019-08-12 17:47:56 +0100

    Add rel='noopener noreferrer' to remove warnings

--- src/components/Film.js
+++ src/components/Film.js
@@ -4,7 +4,7 @@ class Film extends Component {
   render() {
     return(
       <li className="film">
-        <a href={this.props.url} target="_blank">{this.props.children}</a>
+        <a href={this.props.url} target="_blank" rel="noopener noreferrer">{this.props.children}</a>
       </li>
     );
   }
